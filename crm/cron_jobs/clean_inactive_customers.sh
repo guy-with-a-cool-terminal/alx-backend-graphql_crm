9fb61895a7b141197a52a76e495dbfe1e71afbe5
guy-with-a-cool-terminal <njugunabriian@gmail.com>
2025-08-27 02:59:14 +0300

    automation1

--- crm/cron_jobs/clean_inactive_customers.sh
+++ crm/cron_jobs/clean_inactive_customers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/Bri/Desktop/Brian/alx-backend-graphql_crm/alx_backend_graphql
-
+# manage.py
 # django management command to delete inactive customers
 python3 manage.py shell << EOF
 from crm.models import Customer,Order
